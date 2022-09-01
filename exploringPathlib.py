ae5cf58826c793d0b554a132f1b5a03231f39a6d
Sarvesh Wagh <sarvesh.wagh@NLHFD-MB1035.local>
2022-09-01 12:10:01 +0200

    exploringPathlib

---
+++ exploringPathlib.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+p1 = Path("files/file3.txt")
+
+print(p1) # Prints the entire path dir/subdir/filename
+print(p1.name) # Prints the filename ONLY
+print(p1.stem) # Prints the prefix of the filename
+print(p1.suffix) # Prints the suffix/type of the filename
+
+with open(p1, "r") as file:
+    print(file.read())
+
+p2 = Path("files")
+list1 = []
+for item in p2.iterdir():
+    print(item)
+    list1.append(item)
+
+print(list1)
+
+
+
