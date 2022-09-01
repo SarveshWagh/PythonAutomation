5356e99144228613c1f901119247104e83c52d50
Sarvesh Wagh <sarvesh.wagh@NLHFD-MB1035.local>
2022-09-01 12:16:09 +0200

    checking

--- readandwritecontenttofile.py
+++ readandwritecontenttofile.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
-p1 = Path("file2.txt")
+p1 = Path("files/file4.txt")
 if p1.exists():
     with open(p1, "r") as file:
         print(file.read())
 
 else:
     with open(p1, "w") as file:
-        file.write("Adding sample content to the new file, 'file2'.")
+        file.write("Adding sample content to the new file, 'file4'.")
 
 
 print("Testing git changes!!")
